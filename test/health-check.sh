0086cad072789386386c7e7b41e6bc989f4767e4
Dincho Todorov <dincho@users.noreply.github.com>
2021-05-25 18:21:03 +0300

    Revert erroneous S&R changes

--- test/health-check.sh
+++ test/health-check.sh
@@ -22,7 +22,7 @@ curl -sSf -o /dev/null --retry 10 --retry-connrefused http://${API_ADDR}/v3/debu
 # Internal API (dry-run)
 EXT_STATUS=$(curl -sS -o /dev/null --retry 10 --retry-connrefused \
     -X POST -H 'Content-type: application/json' -d '{}' \
-    -w "%{http_code \
+    -w "%{http_code}" \
     http://${API_ADDR}/v2/debug/transactions/dry-run)
 [ $EXT_STATUS -eq 400 ]
 
@@ -35,6 +35,6 @@ EXT_STATUS=$(curl -sS -o /dev/null --retry 10 --retry-connrefused \
 # Temporary disabled - https://github.com/aeternity/aeternity/issues/3131
 # State Channels WebSocket API
 # WS_STATUS=$(curl -sS -o /dev/null --retry 10 --retry-connrefused \
-#     -w "%{http_code \
+#     -w "%{http_code}" \
 #     http://${API_ADDR}/channel)
 # [ $WS_STATUS -eq 426 ]
