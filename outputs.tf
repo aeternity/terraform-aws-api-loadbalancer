e08b851343380a26fe16bd67194d22238f053b44
Michal `acid` Bagrowski <michal@bagrowski.com>
2022-01-27 23:59:21 +0100

    test on multiple terraform versions

--- outputs.tf
+++ outputs.tf
@@ -2,18 +2,18 @@ output "fqdn" {
   value = aws_route53_record.lb.name
 }
 
-output target_groups {
+output "target_groups" {
   value = concat(
-    list(aws_lb_target_group.api_health_check.arn),
-    list(aws_lb_target_group.external_api.arn),
+    [aws_lb_target_group.api_health_check.arn],
+    [aws_lb_target_group.external_api.arn],
     aws_lb_target_group.internal_api.*.arn,
   )
 }
 
-output target_groups_channels {
+output "target_groups_channels" {
   value = aws_lb_target_group.state_channels_api.*.arn
 }
 
-output dns_name {
+output "dns_name" {
   value = aws_lb.api.dns_name
 }
