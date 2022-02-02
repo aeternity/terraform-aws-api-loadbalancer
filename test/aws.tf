2bf7400c3b3652ad75444817b8d2fad5dc86d2ef
Michał <github@bagrowski.com>
2022-02-02 23:06:46 +0100

    bump aws provider (#14)
    * bump aws provider

--- test/aws.tf
+++ test/aws.tf
@@ -1,11 +1,9 @@
 # Default
 provider "aws" {
-  version = "2.33.0"
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 provider "aws" {
-  version = "2.33.0"
-  region  = "ap-southeast-2"
-  alias   = "ap-southeast-2"
+  region = "ap-southeast-2"
+  alias  = "ap-southeast-2"
 }
--- test/health-check.sh
+++ test/health-check.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
 set -Eeuo pipefail
-echo "TEST 1"
 API_ADDR=$(terraform output -json |jq -r '."fqdn"."value"')
-echo "TEST 1"
 echo $API_ADDR
 # Basic health check endpoint
+echo "Basic health check endpoint"
 curl -sSf -o /dev/null --retry 10 --retry-connrefused http://${API_ADDR}/healthz
-echo "TEST 2"
+echo "External api"
 # External API
 curl -sSf -o /dev/null --retry 10 --retry-connrefused http://${API_ADDR}/v2/status
 curl -sSf -o /dev/null --retry 10 --retry-connrefused http://${API_ADDR}/v3/status
-echo "TEST 3"
+echo "Internal API (peers)"
 # Internal API (peers)
 curl -sSf -o /dev/null --retry 10 --retry-connrefused http://${API_ADDR}/v2/debug/peers
 curl -sSf -o /dev/null --retry 10 --retry-connrefused http://${API_ADDR}/v3/debug/peers
-echo "TEST 4"
+echo "Internal API (pending transactions)"
 # Internal API (pending transactions)
 curl -sSf -o /dev/null --retry 10 --retry-connrefused http://${API_ADDR}/v2/debug/transactions/pending
 curl -sSf -o /dev/null --retry 10 --retry-connrefused http://${API_ADDR}/v3/debug/transactions/pending
-echo "TEST 5"
+echo "Internal API (dry-run)"
 # Internal API (dry-run)
 EXT_STATUS=$(curl -sS -o /dev/null --retry 10 --retry-connrefused \
     -X POST -H 'Content-type: application/json' -d '{}' \
     -w "%{http_code}" \
     http://${API_ADDR}/v2/debug/transactions/dry-run)
 [ $EXT_STATUS -eq 400 ]
-echo "TEST 6"
+
 EXT_STATUS=$(curl -sS -o /dev/null --retry 10 --retry-connrefused \
     -X POST -H 'Content-type: application/json' -d '{}' \
     -w "%{http_code}" \
     http://${API_ADDR}/v3/debug/transactions/dry-run)
 [ $EXT_STATUS -eq 400 ]
-echo "TEST 7"
+
 # Temporary disabled - https://github.com/aeternity/aeternity/issues/3131
 # State Channels WebSocket API
 # WS_STATUS=$(curl -sS -o /dev/null --retry 10 --retry-connrefused \
