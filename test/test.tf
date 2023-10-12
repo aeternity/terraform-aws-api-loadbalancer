c95fd5021d7f5085aba05795569110c17f56551b
Dincho Todorov <dincho@users.noreply.github.com>
2023-10-12 14:02:05 +0300

    Update aws provider to v5

--- test/test.tf
+++ test/test.tf
@@ -5,23 +5,32 @@ locals {
 module "test_nodes_sydney" {
   source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=master"
   env               = "test"
-  envid             = var.envid
-  bootstrap_version = var.bootstrap_version
-  vault_role        = "ae-node"
-  vault_addr        = var.vault_addr
 
   static_nodes   = 0
   spot_nodes_min = 1
   spot_nodes_max = 2
 
-  spot_price    = "0.15"
-  instance_type = "t3.large"
-  ami_name      = "aeternity-ubuntu-18.04-*"
+  instance_type  = "t3.large"
+  instance_types = ["t3.large"]
+  ami_name       = "aeternity-ubuntu-18.04-*"
 
   additional_storage      = true
   additional_storage_size = 5
 
   asg_target_groups = module.test_gateway_lb.target_groups
+
+  tags = {
+    role  = "aenode"
+    env   = "test"
+    envid = var.envid
+  }
+
+  config_tags = {
+    bootstrap_version = var.bootstrap_version
+    vault_role        = "ae-node"
+    vault_addr        = var.vault_addr
+    node_config       = "secret/aenode/config/test"
+  }
 
   providers = {
     aws = aws.ap-southeast-2
